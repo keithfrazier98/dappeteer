fbaeea177981233730d3ef1100951744f30a0e0a
Keith Frazier <keithers98@gmail.com>
2022-04-29 12:16:23 -0700

    fix type on jest preset

--- jest-preset.js
+++ jest-preset.js
@@ -1,5 +1,5 @@
 module.exports = {
   globalSetup: `@keithers98/dappeteer-stable/dist/jest/setup.js`,
   globalTeardown: `@keithers98/dappeteer-stable/dist/jest/teardown.js`,
-  testEnvironment: `@keithers98/dappeteer-stable/dist/jest//DappeteerEnvironment.js`,
+  testEnvironment: `@keithers98/dappeteer-stable/dist/jest/DappeteerEnvironment.js`,
 };
